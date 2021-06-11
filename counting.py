bfef1de6cf3bcb531246353ac0b68f96b71987c2
Ryan Walters <RyanWaltersDev@gmail.com>
2021-06-11 15:17:57 -0400

    +finish parrot, add counting

---
+++ counting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# RyanWaltersDev Jun 9 2021 -- while loop
+
+# Initial number and input
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+# END OF PROGRAM
--- parrot.py
+++ parrot.py
@@ -2,7 +2,21 @@
 # RyanWaltersDev Jun 3 2021 -- Initial input() test
 
 # Input message
+'''
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+'''
+
+# New input message
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program "
+
+# while loop -- user input quit
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    # If statement so that quit does not print
+    if message != 'quit':
+        print(message)
 
 # END OF PROGRAM
